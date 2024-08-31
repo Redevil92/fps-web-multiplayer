21f745ecc40a0826b50852cc24731606e4f00a6b
Redevil92 <stefano.badalucco@gmail.com>
2024-08-31 11:34:28 +0200

    Emit player position to all the other player  in the room

--- app/scene/currentPlayer.tsx
+++ app/scene/currentPlayer.tsx
@@ -1,10 +1,23 @@
 "use client";
 
+import { socket } from "@/socket";
 import { Gltf, KeyboardControls } from "@react-three/drei";
 
 import Controller from "ecctrl";
+import {
+  LegacyRef,
+  MutableRefObject,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { Group, Mesh, Object3DEventMap, Vector3 } from "three";
 
 export default function Player() {
+  const ref = useRef<Group<Object3DEventMap>>(null);
+  const [playerSpeed, setPlayerSpeed] = useState(5);
+  const [playerPosition, setPlayerPosition] = useState(new Vector3(0, 10, 0));
+
   const keyboardMap = [
     { name: "forward", keys: ["ArrowUp", "KeyW"] },
     { name: "backward", keys: ["ArrowDown", "KeyS"] },
@@ -15,18 +28,31 @@ export default function Player() {
   ];
 
   function keyboardControlsHandler(name: string, pressed: boolean) {
-    console.log(name, pressed);
+    console.log(name, pressed, ref.current);
+    const playerPosition = ref.current?.getWorldPosition(new Vector3());
+    console.log(socket);
+    socket.emit(
+      "message",
+      { message: socket.id + "-->" + JSON.stringify(playerPosition) },
+      ""
+    );
+  }
+
+  function updateGlftHandler(event: Group<Object3DEventMap>) {
+    console.log("UPDATE", event);
   }
 
   return (
     <KeyboardControls onChange={keyboardControlsHandler} map={keyboardMap}>
-      <Controller maxVelLimit={5}>
+      <Controller maxVelLimit={5} position={playerPosition}>
         <Gltf
+          ref={ref}
           castShadow
           receiveShadow
           scale={0.315}
           position={[0, -0.55, 0]}
           src="/ghost_w_tophat-transformed.glb"
+          onUpdate={updateGlftHandler}
         />
       </Controller>
     </KeyboardControls>

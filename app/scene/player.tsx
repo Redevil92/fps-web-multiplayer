f07f31a754b2a079860e0114641e1570db498d25
Redevil92 <stefano.badalucco@gmail.com>
2024-09-21 10:34:20 +0200

    Fixed collider issue.

--- app/scene/player.tsx
+++ app/scene/player.tsx
@@ -1,9 +1,16 @@
 "use client";
 
-import { Gltf } from "@react-three/drei";
+import { Box, Gltf } from "@react-three/drei";
 import { useFrame, Vector3 } from "@react-three/fiber";
 import { quat, RigidBody } from "@react-three/rapier";
-import { useEffect, useRef, useState } from "react";
+import {} from "@react-three/rapier";
+import {
+  MutableRefObject,
+  RefObject,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 import * as THREE from "three";
 
@@ -14,43 +21,17 @@ interface PlayerProps {
 }
 
 export default function Player({ position, playerId }: PlayerProps) {
-  const testRef = useRef(null);
-  const [newPosition, setNewPosition] = useState(position);
-  const [moveUp, setMoveUp] = useState(false);
-
-  // useEffect(() => {
-  //   if (testRef.current && testRef.current.position) {
-  //     testRef.current.position.x = position.x;
-  //     testRef.current.position.y = position.y;
-  //     testRef.current.position.z = position.z;
-  //   }
-  // }, [position]);
-
-  useEffect(() => {
-    if (testRef.current) {
-      console.log(testRef.current);
-      // const curRotation = quat(testRef.current.rotation());
-      // const incrementRotation = new THREE.Quaternion().setFromAxisAngle(
-      //   new THREE.Vector3(0, 1, 0),
-      //   delta * 2
-      // );
-      //curRotation.multiply(incrementRotation);
-      testRef.current.setNextKinematicTranslation(position);
-    }
-  }, [position]);
-
   return (
     <RigidBody
       key={playerId}
       type="kinematicPosition"
       colliders="trimesh"
-      ref={testRef}
+      position={position}
     >
       <Gltf
         castShadow
         receiveShadow
         scale={0.315}
-        // rotation={rotation}
         src="/ghost_w_tophat-transformed.glb"
       />
     </RigidBody>

acb84d373c358013a968599f894b896f562b1993
Redevil92 <stefano.badalucco@gmail.com>
2024-09-23 21:33:47 +0200

    Added test function

--- app/scene/player.tsx
+++ app/scene/player.tsx
@@ -3,16 +3,28 @@
 import { Gltf } from "@react-three/drei";
 import { Vector3 } from "@react-three/fiber";
 import { RigidBody } from "@react-three/rapier";
+import { useEffect, useRef, useState } from "react";
 
 import * as THREE from "three";
 
 interface PlayerProps {
   position: Vector3;
   playerId: string;
-  rotation: THREE.Euler;
+  rotation: Vector3;
 }
 
 export default function Player({ position, rotation, playerId }: PlayerProps) {
+  const testRef = useRef();
+
+  useEffect(() => {
+    setInterval(function () {
+      // code to be executed repeatedly
+      console.log(position);
+      // const newRotation = new THREE.Euler(rotation.x, rotation.y, rotation.z);
+      //  setEulerAngle(newRotation);
+    }, 1000);
+  }, []);
+
   return (
     <RigidBody
       key={playerId}
